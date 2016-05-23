3d357b8116d3a1e72635782aa14a1a35a3ff2b84
adard <adardayan@gmail.com>
2016-05-23 18:22:11 +0300

    first commit

--- src/map.js
+++ src/map.js
@@ -3,7 +3,8 @@ function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
+Map.prototype.find_a_person = function (name) {
+for(int i=0;i<=this.post.len
     return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
   };
 
