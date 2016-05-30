3f55c844d5d76cd467c3b1193616cc77b4590ff1
sashm <sarahshmueli@gmail.com>
2016-05-30 17:56:21 +0300

    Red: 4 test failed:create by sashm

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -32,7 +32,7 @@ describe('Include location', function() {
 describe('map inconsistencies', function() {
   it('Check if there are map inconsistencies', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
-    var posts = map.map_inconsistencies("posts")
+    var posts = map.map_inconsistencies("aaa")
     expect(posts).to.be.eql(true);
      	
   });
