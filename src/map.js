713474c0ec230a0d45a42c82cea6410fe3356db0
sashm <sarahshmueli@gmail.com>
2016-05-30 17:24:13 +0300

    Green:fist commit

--- src/map.js
+++ src/map.js
@@ -3,17 +3,21 @@ function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function (name) {
-    var flag = "false";
-    for (i = 0; i < this._posts.length; i++) {
-        var n = this._posts[i].indexOf(name);
-        if (n != -1)
-            flag = "true";
-    }
-    if (flag == "true")
-        return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
-    else
-        return [];
+
+//first commit
+Map.prototype.find_a_person = function(name) {
+
+	var flag="false";
+	for(i=0;i<this._posts.length;i++){
+            var n=this._posts[i].indexOf(name);
+	    if(n!=-1)
+		flag="true";		
+	}
+	if(flag=="true")
+ 	 return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
+	else
+	return [];
   };
+
 
 module.exports = Map;
